e6fec58c811c3489a61adf142e3b0cd7910ede34
Glacksy <quazgaming@gmail.com>
2012-05-24 19:04:02 +0200

    added random goblins names

--- CanaryGoblins/src/GoblinsListener.java
+++ CanaryGoblins/src/GoblinsListener.java
@@ -7,13 +7,19 @@ public class GoblinsListener extends PluginListener {
 
 	private static Logger log = Logger.getLogger("Minecraft");
 	
+	static Random generator = new Random();
 	
+	  public static String get (String[] array) {
+	        int rnd = generator.nextInt(array.length);
+	        return array[rnd];
+	    }
 	
 	public void onLogin(Player player) {
-		player.sendMessage("Gmonur greets you, 'Welcome master!'");
+		String[] goblins = { "Gmonur", "Nurezk","Gnutz", "Gkazor", "Snuz", "Rezuxk",  "Drak", "Gexomd", "Zekr", "Regoms"};
+
+		String ret = GoblinsListener.get(goblins);
+		player.sendMessage("~" + ret + "greets you, 'Welcome master!'");
 	}
 	
 }
-
-//test
 
